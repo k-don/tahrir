1be1196899649b6c36defaf7f083cbe4371f30eb
Kieran Donegan <kdonegan.92@gmail.com>
2017-03-20 10:20:43 +0000

    Make karma clear required modules

--- tests.webpack.js
+++ tests.webpack.js
@@ -1,2 +1,9 @@
 var context = require.context('./src/test/js', true, /-test\.js$/);
+var projectModuleIds = context.keys().map(module =>
+    String(context.resolve(module)));
 context.keys().forEach(context);
+
+beforeEach(() => {
+    // Remove our modules from the require cache before each test case.
+    projectModuleIds.forEach(id => delete require.cache[id]);
+});
